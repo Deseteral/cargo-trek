f1acbe3e6174849896917fc28682a22923375ae2
Deseteral <deseteral@gmail.com>
2023-09-30 13:03:06 +0200

    Drawing city names

--- src/scenes/overworld-scene.ts
+++ src/scenes/overworld-scene.ts
@@ -1,6 +1,6 @@
 import { Truck } from 'ludum-dare-54/game/truck';
 import { WorldMap } from 'ludum-dare-54/game/world-map';
-import { Scene, Screen, Vector2, Camera, Input, ENDESGA16PaletteIdx } from 'ponczek';
+import { Scene, Screen, Vector2, Camera, Input, ENDESGA16PaletteIdx, Color } from 'ponczek';
 
 export class OverworldScene extends Scene {
   public worldMap: WorldMap;
@@ -50,18 +50,28 @@ export class OverworldScene extends Scene {
 
     this.camera.begin();
 
+    // Map
     scr.drawTexture(this.worldMap.topoTexture, 0, 0);
     scr.drawTexture(this.worldMap.roadPathTexture, 0, 0);
 
+    // Fog layer
     scr._ctx.drawImage(this.worldMap.fogScreen._domElement, 0, 0);
 
+    // Cities
     for (let i = 0; i < this.worldMap.cities.length; i += 1) {
       const c = this.worldMap.cities[i];
       scr.color(ENDESGA16PaletteIdx[4]);
       scr.fillRect(c.x - 2, c.y - 2, 4, 4);
     }
 
+    // Trucks
     this.truck.render(scr);
+
+    // City names
+    for (let i = 0; i < this.worldMap.cities.length; i += 1) {
+      const c = this.worldMap.cities[i];
+      scr.drawText('City', c.x + 1, c.y + 2, Color.white);
+    }
 
     this.camera.end();
   }
