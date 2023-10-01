883c70d1bfaa2f8bbe3c275379bb55ee22b61d30
Deseteral <deseteral@gmail.com>
2023-10-01 17:53:09 +0200

    Dialog box shadow

--- src/scenes/dialog-box-scene.ts
+++ src/scenes/dialog-box-scene.ts
@@ -6,6 +6,7 @@ export class DialogBoxScene extends Scene {
   heightPx: number;
 
   frameTexture: Texture;
+  frameShadowTexture: Texture;
 
   constructor(text: string) {
     super();
@@ -14,7 +15,9 @@ export class DialogBoxScene extends Scene {
     this.heightPx = text.split('\n').length * (Ponczek.screen.activeFont!.charHeight);
 
     this.frameTexture = Texture.copy(Assets.texture('frame'));
+    this.frameShadowTexture = Texture.copy(Assets.texture('frame'));
 
+    // Frame
     const replaceColorEffect = new ReplaceColorEffect(Color.white, Color.transparent);
     replaceColorEffect.apply(this.frameTexture);
 
@@ -23,6 +26,16 @@ export class DialogBoxScene extends Scene {
 
     replaceColorEffect.set(Color.black, ENDESGA16PaletteIdx[11]);
     replaceColorEffect.apply(this.frameTexture);
+
+    // Frame shadow
+    replaceColorEffect.set(Color.white, Color.transparent);
+    replaceColorEffect.apply(this.frameShadowTexture);
+
+    replaceColorEffect.set(Color.gray, Color.black);
+    replaceColorEffect.apply(this.frameShadowTexture);
+
+    replaceColorEffect.set(Color.black, Color.black);
+    replaceColorEffect.apply(this.frameShadowTexture);
   }
 
   update(): void {
@@ -35,6 +48,7 @@ export class DialogBoxScene extends Scene {
     const x = ((scr.width - this.longestLineLengthPx) / 2) | 0;
     const y = ((scr.height - this.heightPx) / 2) | 0;
 
+    scr.drawNineSlice(this.frameShadowTexture, x + 2, y + 2, this.longestLineLengthPx, this.heightPx, 8, 8);
     scr.drawNineSlice(this.frameTexture, x, y, this.longestLineLengthPx, this.heightPx, 8, 8);
     scr.drawText(this.text, x, y, Color.white);
   }
